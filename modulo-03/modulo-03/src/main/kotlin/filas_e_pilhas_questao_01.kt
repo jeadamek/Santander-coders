a7e3e1921f2a6dffbbfe825f1bec71f13600ee11
Jessica Adamek <jessica.g.adamek@gmail.com>
2023-01-15 19:35:30 -0300

    Update filas_e_pilhas_questao_01.kt

--- modulo-03/modulo-03/src/main/kotlin/filas_e_pilhas_questao_01.kt
+++ modulo-03/modulo-03/src/main/kotlin/filas_e_pilhas_questao_01.kt
@@ -1,18 +1,51 @@
+import java.util.*
 import kotlin.random.Random
 
 data class Message(
     val text: String,
     val recipient: String
-)
+){
+    var sendAttempt: Int = 0
+}
 
 class MessageBroker {
     private val messageSender = Random(1000)
+    val queue: Queue<Message> = LinkedList()
 
     fun processFirstMessage() {
+
         /*
         * Implementar maneira de processar a fila e de retentar o envio de uma mensagem que deu erro
         */
         //send(message)
+        val message = queue.peek()
+        val send = send(message)
+
+        while (!send){
+            message.sendAttempt++
+            if (message.sendAttempt == 3) break;
+        }
+
+        if (message.sendAttempt < 3){
+            println("""
+                Mensagem enviada com sucesso!
+                
+                Mensagem: ${message.text}
+                Destino: ${message.recipient}
+                
+            """.trimIndent())
+            queue.poll()
+        } else {
+            println("""
+                Falha ao enviar a mensagem.
+                
+                Mensagem: ${message.text}
+                Destino: ${message.recipient}
+                
+            """.trimIndent())
+            queue.poll()
+        }
+
     }
 
     /*
@@ -20,5 +53,24 @@ class MessageBroker {
     */
     private fun send(message: Message): Boolean {
         return messageSender.nextBoolean()
+    }
+
+    fun addMessage(message: Message){
+        queue.offer(message)
+    }
+}
+
+fun main() {
+    val message1 = Message("Hello!", "test@test.com")
+    val message2 = Message("Hello there!", "test2@test.com")
+    val message3 = Message("Hello there! Good to see u!", "test3@test.com")
+
+    val messageBroker = MessageBroker()
+    messageBroker.addMessage(message1)
+    messageBroker.addMessage(message2)
+    messageBroker.addMessage(message3)
+
+    while (messageBroker.queue.isNotEmpty()){
+        messageBroker.processFirstMessage()
     }
 }
